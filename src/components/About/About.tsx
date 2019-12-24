e304e694c5d4a6b808f640c08d9805b61b83c94b
Seohyun Yoon <sarahseohyun@gmail.com>
2019-12-25 05:34:22 +0900

    About Markup

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,37 +1,196 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export default class About extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Description> 👩🏻‍💻 </Description>
-        <LinkDescription
-          href='https://github.com/seohyun0120/random-noonsong'
-        >
-          Github
-        </LinkDescription>
-      </Container>
-    )
-  }
+export default function About() {
+  return (
+    <Container>
+      <TextContainer>
+        <Title>
+          Noonsong Factory 🤖
+        </Title>
+        <Description>made by <span>@scorpion</span></Description>
+        <Description>
+          눈송팩토리(Noonsong Factory)는 숙명여자대학교 마스코트인 <span>눈송이</span>를 활용하여 만들어진 Web Application입니다. <br />
+          심심한 숙대생이 남는 시간에 직접 만들어보았습니다. <br />
+          앞으로 계속 추가될 다양한 <span>눈송이</span>테마들을 기대해주세요!
+        </Description>
+      </TextContainer>
+      <ReleaseInfoContainer>
+        <SubTitle>
+          Release Note 🔨
+        </SubTitle>
+        <SubDescription>
+          <h2>v1.0</h2>
+          <h3>December 25, 2019</h3>
+          <h4> 🎉 랜덤눈송이(RandomNoonsong) 출시</h4>
+        </SubDescription>
+      </ReleaseInfoContainer>
+      <HelpContainer>
+        <HelpTitle>
+          사람을 찾아요 👀
+        </HelpTitle>
+        <HelpDescription>
+          <h2>개발하세요?</h2>
+          <h3>
+            <span>NoonsongFactory</span>에 관한 <span>PR & Issue</span>는 언제든지 환영입니다!
+            <a href='https://github.com/seohyun0120/random-noonsong'>
+              Github
+            </a>
+            으로 오세요! 같이 만들어보아요 :)
+          </h3>
+          <h2>눈송이 짤을 그려보신 적 있으신가요?</h2>
+          <h3>
+            눈송템 공구를 해보셨던 총대 학우분들, 눈송이 짤을 그려주시는 숨겨진 학우분들을 찾습니다! <br />
+            <span> sarah5734@sm.ac.kr </span>으로 이메일 보내주세요. <br />
+            눈송팩토리에 다양한 눈송이들이 들어오는 것을 보고싶어요!
+          </h3>
+        </HelpDescription>
+      </HelpContainer>
+    </Container>
+  )
 }
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 5rem;
-  margin-top: auto;
+  text-align: center;
+`
+
+const TextContainer = styled.div`
+  display: inline-block;
+  max-width: 1020px;
+  padding: 3rem 2rem;
+  text-align: left;
+  width: 100%;
+`
+
+const Title = styled.div`
+  display: inline;
+  font-size: 3.125rem;
+  font-weight: 700;
+
+  @media (max-width: 900px) {
+    font-size: 24px;
+  }
 `
 
 const Description = styled.div`
-  font-size: 13px;
+  font-size: 1.225rem;
   font-weight: 500;
+  margin: 1rem 0 0 0;
+  line-height: 1.5;
+
+  span {
+    color: ${({ theme }) => theme.primaryOrange};
+    font-weight: 700;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 13px;
+
+    span {
+      font-size: 14px;
+    }
+  }
 `
 
-const LinkDescription = styled.a`
-  color: #3b5bdb;
-  font-size: 13px;
-  font-weight: 500;
-  text-decoration: none;
-  padding: 0 .125rem; 
+const SubTitle = styled.div`
+  display: inline;
+  font-size: 2.5rem;
+  font-weight: 600;
+  margin: 3rem 0 0 0;
+
+  @media (max-width: 900px) {
+    font-size: 20px;
+  }
+`
+
+const ReleaseInfoContainer = styled.div`
+  display: inline-block;
+  max-width: 1020px;
+  padding: 1rem 2rem;
+  text-align: left;
+  width: 100%;
+`
+
+const SubDescription = styled.div`
+  h2 {
+    font-size: 1.75rem;
+    font-weight: 600;
+  }
+
+  h3 {
+    font-size: 1.325rem;
+    font-weight: 500;
+  }
+
+  h4 {
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    h2 {
+      font-size: 15px;
+    }
+
+    h3 {
+      font-size: 13px;
+    }
+
+    h4 {
+      font-size: 12px;
+    }
+  }
+`
+
+const HelpContainer = styled.div`
+  display: inline-block;
+  max-width: 1020px;
+  padding: 1rem 2rem;
+  text-align: left;
+  width: 100%;
+`
+
+const HelpTitle = styled.div`
+  display: inline;
+  font-size: 2.5rem;
+  font-weight: 600;
+  margin: 3rem 0 0 0;
+
+  @media (max-width: 900px) {
+    font-size: 20px;
+  }
+`
+
+const HelpDescription = styled.div`
+  h2 {
+    font-size: 1.25rem;
+    font-weight: 600;
+  }
+
+  h3 {
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.5;
+  }
+
+  a {
+    margin: 0 .2rem;
+    text-decoration: none;
+    color: ${({ theme }) => theme.primaryGreen};
+  }
+
+  span {
+    color: ${({ theme }) => theme.primaryGreen};
+    font-weight: 600;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    h2 {
+      font-size: 13px;
+    }
+
+    h3 {
+      font-size: 12px;
+    }
+  }
 `
