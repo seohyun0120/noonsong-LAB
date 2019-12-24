02bdeb46b0cf121bdc5b84c63d1c6d0cdc088c9c
Seohyun Yoon <sarahseohyun@gmail.com>
2019-12-25 01:25:11 +0900

    edit global style

--- src/global.ts
+++ src/global.ts
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyles = createGlobalStyle`
   html, body {
     margin: 0;
-    padding: 1rem;
   }
 
   *, *::after, *::before {
@@ -11,13 +10,10 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    align-items: center;
     background: ${({ theme }) => theme.primaryWhite};
     color: ${({ theme }) => theme.primaryBlack};
-    display: flex;
     font-family: -apple-system, BlinkMacSystemFont, 'Malgun Gothic', 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
     height: 100%;
-    justify-content: center;
     text-rendering: optimizeLegibility;
     overflow: hidden;
   }
